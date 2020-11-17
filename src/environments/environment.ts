02c9c7dca1297804342b7206cf466839eae84fce
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-18 00:32:15 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC0bLOgF0CX758I19iaVEys5yt5JSHBQfo",
-    authDomain: "sourd-72398.firebaseapp.com",
-    databaseURL: "https://sourd-72398.firebaseio.com",
-    projectId: "sourd-72398",
-    storageBucket: "sourd-72398.appspot.com",
-    messagingSenderId: "111590180414",
-    appId: "1:111590180414:web:3969c80aefd5f6b8e7736f"
+   
 
   }
 };
